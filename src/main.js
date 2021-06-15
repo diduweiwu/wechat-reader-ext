0833609a2e2bfa4b2dc69d2ce98e011531563fbc
gaoyuan <gaoyuan@fiture.com>
2021-06-15 22:38:00 +0800

    更新组件为动态引入，减少构建js包体积

--- src/main.js
+++ src/main.js
@@ -1,9 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.use(ElementUI)
+import 'element-ui/lib/theme-chalk/index.css';
+import {
+  Input,
+  Radio,
+  RadioGroup,
+  Button,
+  Popover,
+  Tooltip,
+  Form,
+  FormItem,
+  Badge,
+  Card,
+  Divider,
+} from 'element-ui';
+
+
+Vue.use(Input)
+Vue.use(Radio)
+Vue.use(RadioGroup)
+Vue.use(Button)
+Vue.use(Popover)
+Vue.use(Tooltip)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Badge)
+Vue.use(Card)
+Vue.use(Divider)
 
 // 挂载节点
 const monkey_plugin = document.createElement("div")
